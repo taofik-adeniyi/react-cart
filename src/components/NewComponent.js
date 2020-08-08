c4292d901a5ea2a2217fc30fd32b9ae49d75f280
taofik-adeniyi <bidemi64@gmail.com>
2020-08-08 14:39:51 +0100

    created a new component

--- src/components/NewComponent.js
+++ src/components/NewComponent.js
@@ -4,7 +4,7 @@ class NewComponent extends React.Component {
     render() {
         return (
             <div>
-                
+                <h1>Hello This is a new Component</h1>                
             </div>
         )
     }
